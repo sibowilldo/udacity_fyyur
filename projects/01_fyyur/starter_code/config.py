12dd116a86d9b1a441511eca3e2393121cf9b4aa
Sibongiseni Msomi <19819312+sibowilldo@users.noreply.github.com>
2022-08-14 15:16:27 +0200

    Venues (Index, Show)
    Venues Index and Show Pages and Actions

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,5 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Password01!@localhost:5432/udacity_fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Password01!@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
